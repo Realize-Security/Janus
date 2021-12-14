f1dfc9464c293eb3caaa5c6fae75233e9ae14b97
Richard <richard@realizesec.com>
2021-12-14 09:08:52 -0800

    refactored docker and run script

--- web/scripts/run.sh
+++ web/scripts/run.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-CMD='gunicorn --bind 0.0.0.0:5000 manage:app'
-
-if [[ -z $DEBUG_ENV ]]; then
-  $CMD
+if [ -z "$FLASK_DEBUG" ]; then
+  python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m flask run --host=0.0.0.0
+elif [ -z "$FLASK_DEV" ]; then
+  python -m flask run --host=0.0.0.0
 else
-  python -m debugpy --listen 0.0.0.0:5678 --wait-for-client --multiprocess -m $CMD
+  gunicorn --bind 0.0.0.0:5000 manage:app
 fi
-# 
